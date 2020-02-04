f04611c3ff170c1364b82267e9f2fdc482c16131
Pedro Henrique da Silva Moreira <moreiraphs@outlook.com>
2020-02-04 12:40:44 -0300

    ADD.: add new task and delete task

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,57 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <NewTask @taskAdded="addTask" />
+    <TaskGrid :tasks="tasks" @taskDeleted="deleteTask" />
+  </div>
 </template>
 
 <script>
+import TaskGrid from "./components/TaskGrid";
+import NewTask from "./components/NewTask";
 export default {
-
-}
+  components: { TaskGrid, NewTask },
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  methods: {
+    addTask(task) {
+      const sameName = t => t.name === task.name;
+      const reallyNew = this.tasks.filter(sameName).length === 0;
+      reallyNew &&
+        this.tasks.push({
+          name: task.name,
+          pending: task.pending || true
+        });
+    },
+    deleteTask(i) {
+      this.tasks.splice(i, 1);
+    }
+  }
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 </style>
